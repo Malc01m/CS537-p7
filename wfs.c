f3781115d3d6d30ae8632bf934495d99803e7c93
benjamin-wirch <bwirch@rockhopper-07.cs.wisc.edu>
2024-04-30 19:06:44 -0500

    first implementation of wfs_write

--- wfs.c
+++ wfs.c
@@ -8,6 +8,7 @@
 int debug_statements = 0;
 
 char *disk_img = NULL;
+struct wfs_sb *sb;
 
 int get_inode(const char *path, struct wfs_inode *inode)
 {
@@ -104,15 +105,15 @@ static int wfs_read(const char *path, char *buf, size_t size, off_t offset, stru
         else if (block_index >= D_BLOCK && block_index < N_BLOCKS) {
             // Indirect block reading
             off_t indirect_index = block_index - D_BLOCK;
-            off_t *indirect_block = (off_t *)(disk_img + inode.blocks[IND_BLOCK] * BLOCK_SIZE);
+            off_t *indirect_block = (off_t *)(disk_img + sb->d_blocks_ptr + inode.blocks[IND_BLOCK] * BLOCK_SIZE);
             off_t data_block_addr = indirect_block[indirect_index];
-            char *src = disk_img + data_block_addr * BLOCK_SIZE + block_offset;
+            char *src = disk_img +  sb->d_blocks_ptr + data_block_addr * BLOCK_SIZE + block_offset;
             memcpy(buf, src, read_remain);
         }
         else {
             // idk how it would ever get here but just in case...
             printf("ERROR");
-            return(1);
+            return 1;
         }
 
         buf += read_remain;
@@ -123,10 +124,62 @@ static int wfs_read(const char *path, char *buf, size_t size, off_t offset, stru
     return total_read;
 }
 
-// Placeholder
 static int wfs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
-    return 0; // Return 0 on success
+    struct wfs_inode inode;
+
+    if (get_inode(path, &inode) != 0) {
+        return -ENOENT;
+    }
+
+    size_t total_write = 0;
+    int fd = open (disk_img, O_RDWR);
+
+    while (size > 0) {
+        off_t block_index = offset / BLOCK_SIZE;
+        off_t block_offset = offset % BLOCK_SIZE;
+        size_t bytes_in_block = BLOCK_SIZE - block_offset;
+        size_t write_amount = (size < bytes_in_block) ? size : bytes_in_block;
+
+        if (block_index < D_BLOCK) {
+            char *dest = disk_img + inode.blocks[block_index] * BLOCK_SIZE + block_offset;
+            memcpy(dest, buf, write_amount);
+        }
+        else if (block_index >= D_BLOCK && block_index < N_BLOCKS) {
+            off_t indirect_index = block_index - D_BLOCK;
+            off_t *indirect_block = (off_t *)(disk_img + inode.blocks[IND_BLOCK] * BLOCK_SIZE);
+            off_t data_block_addr = indirect_block[indirect_index];
+            char *dest = disk_img + data_block_addr * BLOCK_SIZE + block_offset;
+            memcpy(dest, buf, write_amount);
+        }
+        else {
+            close(fd);
+            printf("ERROR: ENOSPC wfs_write");
+            return -ENOSPC;
+        }
+
+        buf += write_amount;
+        offset += write_amount;
+        total_write += write_amount;
+        size -= write_amount;
+    }
+
+    // Update inode size & mtime
+    if (offset > inode.size) {
+        inode.size = offset;
+    }
+    inode.mtim = time(NULL);
+
+    off_t inode_offset = sb->i_blocks_ptr + (inode.num * sizeof(struct wfs_inode));
+    if (pwrite(fd, &inode, sizeof(struct wfs_inode), inode_offset) != sizeof(struct wfs_inode)) {
+        printf("ERROR: writeback fail");
+        close(fd);
+        return 1;
+    }
+
+    close(fd);
+
+    return total_write;
 }
 
 // Placeholder
